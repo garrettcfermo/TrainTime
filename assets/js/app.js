056c7d7611b45bb388ff5de89bdba66dcaba7982
Garrett Fermo <gfermo@prpllc.com>
2018-09-14 17:59:11 -0700

    Created Firebase Event

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,35 +21,70 @@
     // Grabs Train Info
     var trainName = $('#trainName').val().trim()
     var trainDest = $('#destination').val().trim()
-    var trainFirstTime = moment($('#firstTrainTime').val().trim(),"hh:mm").format("X")
+    var trainFirstTime = moment($('#firstTrainTime').val().trim(),"HH:mm").format("X")
     var trainFreq = $('#frequencyMin').val().trim()
 
+    
     // Combining into a Object
     var trainInfo ={
       name: trainName,
       destination: trainDest,
-      first_time: trainFirstTime,
+      firstTime: trainFirstTime,
       frequency: trainFreq
+    } 
+
+    // Uploads Train Information to Database
+    db.ref().push(trainInfo)
+
+  })
+
+  // Firebase Event
+
+  db.ref().on("child_added",function(data){
+    console.log(data.val())
+
+    var trainName = data.val().name
+    var trainDest = data.val().destination
+    var trainFirstTime = data.val().firstTime
+    var trainFreq = data.val().frequency
+
+    // Current Time
+    var currentTime = moment().format("X")
+
+    console.log(moment.unix(trainFirstTime).format("MM/DD/YYYY hh:mm A"))
+    console.log(moment.unix(currentTime).format("MM/DD/YYYY hh:mm A"))
+
+
+    // Minutes from the Strat Time to Current Time
+    var minArrival = moment().diff(moment(trainFirstTime,"X"), 'minutes')
+    var minLast = minArrival % trainFreq
+    var awayTrain = 0
+
+    console.log(minArrival)
+
+
+    // Determines Minutes away from Next Train
+    if (minArrival < 0) {
+      awayTrain = (-minArrival) + 1
+    } else {
+      awayTrain = (trainFreq - minLast)
     }
+
+    // Determines Next Arrival
+    var nextArrival = moment().add(awayTrain, 'minutes').format("hh:mm A")
+
+      console.log(awayTrain)
+      console.log(nextArrival)
+
+    
+    // Create the new row
+    
+
+    // Append the new row to the table
 
 
   })
 
-var ftime= moment("10:00","HH:mm")
-console.log("Start Time:" +ftime)
-
-var ctime=moment()
-console.log("Current Time: " + ctime)
 
 
-var minArrival = ctime.diff(ftime,'minutes')
-var minLast= minArrival % 10
-var awayTrain = 10 - minLast
 
-console.log(minArrival)
-console.log(minLast)
-console.log(awayTrain)
-
-var nextArrival = ctime.add(awayTrain,'minutes').format("hh:mm A")
-
-console.log(nextArrival)
